89c90a3ccedcb55cc4ceb813487da2db4adafd41
wbeckler <wbeckler@gmail.com>
2024-05-22 17:05:41 -0400

    erver

--- server/main.gd
+++ server/main.gd
@@ -1,11 +1,25 @@
 extends Node3D
 
-
+var server = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	server = ENetMultiplayerPeer.new()
+	server.create_server(12345)
+	get_tree().set_network_peer(server)
+	server.connect("peer_connected", self, "_on_peer_connected")
+	server.connect("peer_disconnected", self, "_on_peer_disconnected")
+	print("Server started on port 12345")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+	
+
+func _on_peer_connected(id):
+	print("Peer connected: %d" % id)
+
+func _on_peer_disconnected(id):
+	print("Peer disconnected: %d" % id)
