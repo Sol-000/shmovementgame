0b2633f8cf7f2348aea15b261c75b6414859af23
wbeckler <wbeckler@gmail.com>
2024-05-22 17:13:27 -0400

    client

--- main.gd
+++ main.gd
@@ -1,11 +1,36 @@
 extends Node3D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+
+var client = null
+
+func _ready():
+	start_client()
+
+func start_client():
+	client = ENetMultiplayerPeer.new()
+	var result = client.create_client("127.0.0.1", 12345)  # Server IP and port number
+	if result != OK:
+		print("Failed to connect to server")
+		return
+	get_tree().set_network_peer(client)
+	client.connect("connection_succeeded", self, "_on_connection_succeeded")
+	client.connect("connection_failed", self, "_on_connection_failed")
+	client.connect("connection_error", self, "_on_connection_error")
+	print("Client attempting to connect to server at 127.0.0.1:12345")
+
+func _on_connection_succeeded():
+	print("Successfully connected to the server")
+
+func _on_connection_failed():
+	print("Failed to connect to the server")
+
+func _on_connection_error():
+	print("Connection error")
